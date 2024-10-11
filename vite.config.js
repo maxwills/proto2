d10ea4ca153f7cef975b121922b253fc0a4735e1
MaxW <maxwills@gmail.com>
2024-10-11 16:52:27 +0200

    second commit

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 
 // Replace 'your-username' and 'your-repository' with your actual GitHub username and repository name
 export default defineConfig({
-  base: '/proto2/', // This should match your GitHub repository name
+  base: '/proto2/', // This should match your GitHub repository m
+  build: {
+    outDir: 'docs', // Output directory for build files
+  },
 })
